cc499b0428956cd5577ec96748edffa14f1e6a6c
kaweromero <kaweromero@gmail.com>
2015-12-05 23:40:33 -0300

    Uns mains doidos

---
+++ CompiladoresUEPB/src/testeJTree/Main.java
@@ -0,0 +1,19 @@
+package testeJTree;
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Digite o código de uma classe de entrada");
+		System.out.print("Lembrando que o código deve seguir as regras gramaticais explícitas no projeto: ");
+		System.out.println();
+		Analisador analisador = new Analisador(System.in);
+		
+		try {
+			analisador.Start();
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro.");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
