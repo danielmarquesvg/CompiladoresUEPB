647a5a9d3645d3b52ad5be8649cc7ad93fc8ed00
kaweromero <kaweromero@gmail.com>
2015-12-05 22:36:34 -0300

    MUDANÇA RADICAL

--- CompiladoresUEPB/src/sintaxe/Type.java
+++
@@ -1,9 +0,0 @@
-package sintaxe;
-
-import visitor.Visitor;
-import visitor.TypeVisitor;
-
-public abstract class Type {
-	  public abstract void accept(Visitor visitor);
-	  public abstract Type accept(TypeVisitor visitor);
-	}
